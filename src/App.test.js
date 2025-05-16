15c365a6c5d057ea886fc025025570c23fee8f2a
DIT\ditmira <ditmiragjini@gmail.com>
2025-05-16 19:51:54 +0200

    initial changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import BingoApp from './BingoApp';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<BingoApp />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
